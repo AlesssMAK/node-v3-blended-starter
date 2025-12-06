f4dd31001170e46f8a1c264882cface4cd5391e2
Alexey <makovii88@gmail.com>
2025-12-06 16:16:25 +0100

    added avatar

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,23 @@
+import createHttpError from 'http-errors';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { User } from '../models/user.js';
 
 export const updateUserAvatar = async (req, res) => {
-    res.status(200).json()
-}
+  console.log(req.file);
+
+  if (!req.file) {
+    throw createHttpError(400, 'No file');
+  }
+
+  const result = await saveFileToCloudinary(req.file.buffer);
+
+  const user = await User.findByIdAndUpdate(
+    '693438b387182bcbd23eef94',
+    { avatar: result.secure_url },
+    { new: true },
+  );
+
+  console.log(user);
+
+  res.status(200).json({ url: user.avatar });
+};
